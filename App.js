bfe1c0b9558acbc861f16eb94824e835187d8229
crively <felipecirveli83@gmail.com>
2025-10-20 15:40:10 -0300

    Feat: funcionalidade navigation

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { SafeAreaView } from 'react-native';
-import AppContainer from './src/AppContainer';
+import { NavigationContainer } from '@react-navigation/native';
+import Navigation from './src/main/Navigation';
 
 
 export default function App() {
 return (
-<SafeAreaView style={{ flex: 1 }}>
-<AppContainer />
-</SafeAreaView>
+<NavigationContainer>
+<Navigation />
+</NavigationContainer>
 );
 }
--- src/main/Navigation.js
+++ src/main/Navigation.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import HomeScreen from '../screens/HomeScreen';
-import Agendamento from '../screens/agendamento';
-import MinhaConta from '../screens/minhaConta';
+import HomeScreen from '../screens/homeScreen.js';
+import Agendamento from '../screens/agendamento.js';
+import MinhaConta from '../screens/minhaConta.js';
 
 
 const Stack = createNativeStackNavigator();
@@ -11,9 +11,9 @@ const Stack = createNativeStackNavigator();
 export default function Navigation() {
 return (
 <Stack.Navigator>
-<Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Corte Em Dia' }} />
-<Stack.Screen name="Booking" component={Agendamento} options={{ title: 'Agendar Corte' }} />
-<Stack.Screen name="Account" component={MinhaConta} options={{ title: 'Minha Conta' }} />
+<Stack.Screen name="Home" component={HomeScreen}/>
+<Stack.Screen name="Booking" component={Agendamento}/>
+<Stack.Screen name="Account" component={MinhaConta}/>
 </Stack.Navigator>
 );
 }
