f3020894660dd9f4b52ce943956281039c12e967
marcosbr29 <marcosbr29@Hotmail.com>
2025-11-07 18:31:56 -0300

    pasta koks e nodeles para rodar o npm start

---
+++ src/hooks/useAuth.js
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+export default function useAuth() {
+  const [user, setUser] = useState(null);
+
+  const login = (email, senha) => {
+    setUser({ email });
+  };
+
+  const logout = () => setUser(null);
+
+  return { user, login, logout };
+}
